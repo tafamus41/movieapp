007ba8652c37b90311efa02ed1a4c7543de96a7c
asimov <seyranmustafa4@gmail.com>
2024-11-11 02:12:10 +0300

    p

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Main from "../pages/Main";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import MovieDetail from "../pages/MovieDetail";
+import Navbar from "../components/Navbar";
+import PrivateRouter from "./PrivateRouter";
+
+const AppRouter = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="" element={<PrivateRouter />}>
+          <Route path="/details/:movieId" element={<MovieDetail />} />
+        </Route>
+      </Routes>
+    </>
+  );
+};
+
+export default AppRouter;
